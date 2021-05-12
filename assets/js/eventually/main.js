f37517a6dd9999e7cc5d7c54f787f99c86312fd0
tejozymandias <36965842+tejozymandias@users.noreply.github.com>
2021-05-12 12:01:21 +0530

    Update main.js

--- assets/js/eventually/main.js
+++ assets/js/eventually/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-aptrinsic('set', 'globalContext', {"productOne":"eventually-One"});
+aptrinsic('set', 'globalContext', {"productTwo":"eventually-Two"});
 
 
 (function() {
